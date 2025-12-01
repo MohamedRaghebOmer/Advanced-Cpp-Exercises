5b462ff9715adada951fb04f9b8c1f1f1259052b
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2025-12-01 05:07:39 +0200

    setup \'Double Linked List Exercises\' project and prepare workspace

---
+++ Double Linked List Exercises/Double Linked List Exercises.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
---
+++ Double Linked List Exercises/clsDblLinkedList.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template <class type> class clsDblLinkedList
+{
+
+};
+
